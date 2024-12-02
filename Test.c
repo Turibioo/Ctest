de4dfa87d8dede6366ff5f0f012a0e9ade9fb50d
Turibioo <arthturibio@gmail.com>
2024-12-02 22:28:06 +0000

    teste com o C

---
+++ Test.c
@@ -0,0 +1,8 @@
+int num1;
+
+    printf("Insira um numero: ");
+    scanf("%d", num1);
+        if(num1<10)
+            printf("numero menor q 10");
+        else
+            printf("numero mais q 10");
